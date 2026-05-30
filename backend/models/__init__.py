7b0b21997a780b7dfa9f6bcc3ef51bc8ba9faa3d
Khoa Nguyen <khoadangnguyen2012@gmail.com>
2026-05-31 01:54:28 +0700

    feat(db): add user sessions table and database migrations

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,6 +1,6 @@
-from auth.model import User
+from auth.model import User, UserSession
 from invites.model import Invitation
 from kit.database import Base
 from spaces.model import Space, SpaceMember
 
-__all__ = ["Base", "Invitation", "Space", "SpaceMember", "User"]
+__all__ = ["Base", "Invitation", "Space", "SpaceMember", "User", "UserSession"]
